b15a2e194bf4392af68e7412c680328aaeeccda5
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-01-02 21:02:14 +0000

    KAT test for LUT4
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   tests/kat/Makefile
    	new file:   tests/kat/lut4.c

---
+++ tests/kat/lut4.c
@@ -0,0 +1,61 @@
+
+/* Known answer tests for the LUT4 instruction. Used to check that the
+    simulator / thing operating it implements it correctly with some
+    degree of confidence.
+*/
+
+#include <stdio.h>
+
+
+int test_lut4() {
+
+    uint32_t rs1 = 0x01234567;
+    uint32_t rs2 = 0x89ABCDEF;
+
+    uint32_t rd  = 0x01234567;
+
+    // TEST 1
+
+    __asm__("lut4 %0, %1, %2" : "+r"(rd): "r"(rs1), "r"(rs2));
+
+    if(rd != 0xFEDCBA98) {
+        return 1;
+    }
+
+
+    // TEST 2
+
+    rd = 0x89ABCDEF;
+
+    __asm__("lut4 %0, %1, %2" : "+r"(rd): "r"(rs1), "r"(rs2));
+
+    if(rd != 0x76543210) {
+        return 2;
+    }
+
+    // END of tests.
+
+    return 0;
+
+}
+
+int main (int argc, char ** argv) {
+
+    printf("Running lut4 KAT...\n");
+
+    int fail = test_lut4();
+
+    if(fail == 0)  {
+
+        printf("Test passed.\n");
+
+        return 0;
+
+    } else {
+        
+        printf("Test %d Failed.\n", fail);
+
+        return 1;
+    }
+
+}
