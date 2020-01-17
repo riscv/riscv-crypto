d0a7dc75bffc8cd44720e3839af6a0d546f3e30f
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-01-17 12:11:59 +0000

    SSHA3 intrinsics
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   share/riscv-crypto-intrinsics.h

--- benchmarks/share/riscv-crypto-intrinsics.h
+++ benchmarks/share/riscv-crypto-intrinsics.h
@@ -20,7 +20,7 @@
 #endif
 
 //
-// SHA2
+// SHA256
 //
 
 #if (defined(__ZSCRYPTO) && (defined(RISCV_CRYPTO_RV32) || defined(RISCV_CRYPTO_RV64)))
@@ -35,6 +35,10 @@ static inline uint32_t _ssha256_s2 (uint32_t rs1) {return (ROTR32(rs1, 7) ^ ROTR
 static inline uint32_t _ssha256_s3 (uint32_t rs1) {return (ROTR32(rs1,17) ^ ROTR32(rs1,19) ^    SHR(rs1,10));}
 #endif
 
+//
+// SHA512
+//
+
 #if defined(__ZSCRYPTO) && defined(RISCV_CRYPTO_RV64)
 static inline uint64_t _ssha512_s0 (uint64_t rs1) {uint64_t rd; __asm__ ("ssha512.s0  %0, %1" : "=r"(rd) : "r"(rs1)); return rd;}
 static inline uint64_t _ssha512_s1 (uint64_t rs1) {uint64_t rd; __asm__ ("ssha512.s1  %0, %1" : "=r"(rd) : "r"(rs1)); return rd;}
@@ -45,6 +49,24 @@ static inline uint64_t _ssha512_s0 (uint64_t rs1) {return (ROTR64(rs1,28) ^ ROTR
 static inline uint64_t _ssha512_s1 (uint64_t rs1) {return (ROTR64(rs1,14) ^ ROTR64(rs1,18) ^ ROTR64(rs1,41));}
 static inline uint64_t _ssha512_s2 (uint64_t rs1) {return (ROTR64(rs1, 1) ^ ROTR64(rs1, 8) ^ SHR(rs1,7))    ;}
 static inline uint64_t _ssha512_s3 (uint64_t rs1) {return (ROTR64(rs1,19) ^ ROTR64(rs1,61) ^ SHR(rs1,6))    ;}
+#endif
+
+//
+// SHA3
+//
+
+#if (defined(__ZSCRYPTO) && (defined(RISCV_CRYPTO_RV32) || defined(RISCV_CRYPTO_RV64)))
+static inline int _ssha3_xy (int x, int y) { int rd; __asm__("ssha3.xy %0, %1, %2" : "=r"(rd) : "r"(rs1), "r"(rs2)); return rd;}
+static inline int _ssha3_x1 (int x, int y) { int rd; __asm__("ssha3.x1 %0, %1, %2" : "=r"(rd) : "r"(rs1), "r"(rs2)); return rd;}
+static inline int _ssha3_x2 (int x, int y) { int rd; __asm__("ssha3.x2 %0, %1, %2" : "=r"(rd) : "r"(rs1), "r"(rs2)); return rd;}
+static inline int _ssha3_x4 (int x, int y) { int rd; __asm__("ssha3.x4 %0, %1, %2" : "=r"(rd) : "r"(rs1), "r"(rs2)); return rd;}
+static inline int _ssha3_yx (int x, int y) { int rd; __asm__("ssha3.yx %0, %1, %2" : "=r"(rd) : "r"(rs1), "r"(rs2)); return rd;}
+#else
+static inline int _ssha3_xy (int x, int y) { return ((x  )%5)+(5*((      y)%5));}
+static inline int _ssha3_x1 (int x, int y) { return ((x+1)%5)+(5*((      y)%5));}
+static inline int _ssha3_x2 (int x, int y) { return ((x+2)%5)+(5*((      y)%5));}
+static inline int _ssha3_x4 (int x, int y) { return ((x+4)%5)+(5*((      y)%5));}
+static inline int _ssha3_yx (int x, int y) { return ((y  )%5)+(5*((2*x+3*y)%5));}
 #endif
 
 #endif
