a94b17510065e02c54d5119816dde56abc5997c7
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-01-21 14:05:47 +0000

    Remove info printing from tools/clone.sh

--- tools/clone.sh
+++ tools/clone.sh
@@ -2,24 +2,6 @@
 
 source $REPO_HOME/tools/share.sh
 
-echo "Setting up toolchain..."
-echo "---------------------------------------------------"
-echo "Download Dir    : $DIR_BASE"
-echo "Installation Dir: $INSTALL_DIR"
-echo "Target Arch     : $TARGET_ARCH / $ARCH_STRING / $ABI_STRING"
-echo "GCC Commit      : $COMMIT_GCC"
-echo "Binutils Commit : $COMMIT_BINUTILS"
-echo "Spike Commit    : $COMMIT_SPIKE"
-echo ""
-echo "DIR_GCC         = $DIR_GCC"
-echo "DIR_BINUTILS    = $DIR_BINUTILS"
-echo "DIR_NEWLIB      = $DIR_NEWLIB"
-echo "DIR_PK          = $DIR_PK"
-echo "DIR_SPIKE       = $DIR_SPIKE"
-echo ""
-echo "Branch Name     = $BRANCH_NAME"
-echo "---------------------------------------------------"
-
 set -e
 set -x
 
