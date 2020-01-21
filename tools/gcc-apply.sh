47e64e991f5fca93afc2957fde31ec10252c9fe5
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-01-21 14:06:01 +0000

    Fix GCC patch apply script!

--- tools/gcc-apply.sh
+++ tools/gcc-apply.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+source $REPO_HOME/tools/share.sh
+
+set -e
+set -x
+
+# ------ GCC ---------------------------------------------------------------
+
+cd           $DIR_GCC
+git apply    $PATCH_GCC
+git add      --all
+
