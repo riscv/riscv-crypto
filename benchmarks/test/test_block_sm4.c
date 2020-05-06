a3faa5800cfbe1437100805ae6706f0284ef4eb9
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-05-06 16:48:07 +0100

    Benchmarks: SM4: Tidy
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   benchmarks/test/test_block_sm4.c
    
     Changes not staged for commit:
    	modified:   extern/riscv-gnu-toolchain (modified content)
    	modified:   extern/riscv-isa-sim (modified content)

--- benchmarks/test/test_block_sm4.c
+++ benchmarks/test/test_block_sm4.c
@@ -27,6 +27,8 @@ int main(int argc, char ** argv) {
     
     printf("import sys, binascii, Crypto.Cipher.AES as AES\n");
     printf("benchmark_name = \"" STR(TEST_NAME)"\"\n");
+    
+    uint64_t start_instrs;
 
     for(int test = 0; test < 1; test ++) {
 
@@ -38,32 +40,33 @@ int main(int argc, char ** argv) {
         uint32_t  erk[32];
         uint32_t  drk[32];
 
+        start_instrs        = test_rdinstret();
         sm4_key_schedule_enc(erk, mk);
-        sm4_key_schedule_dec(drk, mk);
-        sm4_block_enc_dec   (ct,pt,erk);
-        sm4_block_enc_dec   (fi,ct,drk);
-        
-        printf("pt  = ");
-        puthex_py(pt, 16);
-        printf("\n");
+        uint64_t kse_icount = test_rdinstret() - start_instrs;
 
-        printf("mk  = ");
-        puthex_py(mk, 16);
-        printf("\n");
+        start_instrs        = test_rdinstret();
+        sm4_key_schedule_dec(drk, mk);
+        uint64_t ksd_icount = test_rdinstret() - start_instrs;
+
+        start_instrs        = test_rdinstret();
+        sm4_block_enc_dec   (ct,pt,erk);
+        uint64_t enc_icount = test_rdinstret() - start_instrs;
+
+        start_instrs        = test_rdinstret();
+        sm4_block_enc_dec   (fi,ct,drk);
+        uint64_t dec_icount = test_rdinstret() - start_instrs;
         
-        printf("erk = ");
-        puthex_py((uint8_t*)erk , 32*4);
-        printf("\n");
-        printf("drk = ");
-        puthex_py((uint8_t*)drk , 32*4);
-        printf("\n");
+        printf("pt  = "); puthex_py(pt, 16); printf("\n");
+        printf("mk  = "); puthex_py(mk, 16); printf("\n");
+        printf("erk = "); puthex_py((uint8_t*)erk , 32*4); printf("\n");
+        printf("drk = "); puthex_py((uint8_t*)drk , 32*4); printf("\n");
+        printf("ct  = "); puthex_py(ct, 16); printf("\n");
+        printf("fi  = "); puthex_py(fi, 16); printf("\n");
         
-        printf("ct  = ");
-        puthex_py(ct, 16);
-        printf("\n");
-        printf("fi  = ");
-        puthex_py(fi, 16);
-        printf("\n");
+        printf("kse_icount = 0x"); puthex64(kse_icount); printf("\n");
+        printf("ksd_icount = 0x"); puthex64(ksd_icount); printf("\n");
+        printf("enc_icount = 0x"); puthex64(enc_icount); printf("\n");
+        printf("dec_icount = 0x"); puthex64(dec_icount); printf("\n");
 
         int tr = 0;
         for(int i = 0; i < 16; i ++) {
@@ -82,7 +85,12 @@ int main(int argc, char ** argv) {
             printf("sys.exit(1)\n", test, tr);
             return tr;
         } else {
-            printf("print('"STR(TEST_NAME)" Test %d passed.')\n", test);
+        printf("sys.stdout.write(\""STR(TEST_NAME)" Test passed. \")\n");
+        printf("sys.stdout.write(\"enc: %%d, \" %% (enc_icount))\n");
+        printf("sys.stdout.write(\"dec: %%d, \" %% (dec_icount))\n");
+        printf("sys.stdout.write(\"kse: %%d, \" %% (kse_icount))\n");
+        printf("sys.stdout.write(\"ksd: %%d, \" %% (ksd_icount))\n");
+        printf("print(\"\")\n");
         }
     }
 
